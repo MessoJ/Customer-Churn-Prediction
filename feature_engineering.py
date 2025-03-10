df18377811bdacfc24e0c32fb139586737dacf56
Meso Francis <118773373+MessoJ@users.noreply.github.com>
2025-03-10 03:42:01 +0300

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -4,7 +4,7 @@ def engineer_features():
     df = pd.read_csv('data/processed_data.csv')
     
     # Use correct column names from preprocessing
-    df = pd.read_csv('telco_churn.csv')
+    df = pd.read_csv('data/telco_churn.csv')
     print(df.columns)  # Print the available columns
     df['monthly_charges'] = df['monthly_charges'].astype(float)
     df['TotalCharges'] = df['TotalCharges'].astype(float)
